169746902def917957fd9cded10c5c94b2919abf
Victor Hugo Mantovani <victormantova@gmail.com>
2021-02-26 00:27:03 -0300

    add raindrops solution

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,16 @@
 def convert(number):
-    pass
+    result = ''
+
+    if number % 3  == 0:
+        result += 'Pling'
+
+    if number % 5 == 0:
+        result += 'Plang'
+
+    if number % 7 == 0:
+        result += 'Plong'
+
+    if not result:
+        result += str(number)
+    
+    return result
